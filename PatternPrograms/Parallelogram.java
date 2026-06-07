c0170d16f5c3c7eb54b23c597803fcf19a287356
darshan335 <darshan4106@gmail.com>
2026-06-07 18:05:28 +0530

    Added Parallelogram.java file

---
+++ PatternPrograms/Parallelogram.java
@@ -0,0 +1,22 @@
+package com.gqt;
+
+import java.util.Scanner;
+
+public class Parallelogram {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = scan.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int k = 1; k <= i; k++) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= n; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
