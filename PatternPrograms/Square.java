dde850b884118b91bf153ebf54d3f2c63acbf180
darshan335 <darshan4106@gmail.com>
2026-06-07 18:02:32 +0530

    Added Square.java file

---
+++ PatternPrograms/Square.java
@@ -0,0 +1,28 @@
+package com.gqt;
+
+import java.util.Scanner;
+
+public class Square {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = scan.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+// Output
+//-------------------------
+//Enter the number of rows : 5
+//* * * * * 
+//* * * * * 
+//* * * * * 
+//* * * * * 
+//* * * * * 
