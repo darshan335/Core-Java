1b9b914a644168ced496ebb0d08a5b455e9bd82a
darshan335 <darshan4106@gmail.com>
2026-06-09 21:15:07 +0530

    Added Right Angle Traiangle Number.java

---
+++ PatternPrograms/RightAngleTriangleNumber.java
@@ -0,0 +1,27 @@
+package com.gqt;
+
+import java.util.Scanner;
+
+public class RightAngleTriangleNumber {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = scan.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int k = 0; k <i; k++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+//Output
+//----------
+//Enter the number of rows : 5
+//1 
+//2 2 
+//3 3 3 
+//4 4 4 4 
+//5 5 5 5 5 
