3f943d3571c4a65040e02d2c3ce1155683f7874e
darshan335 <darshan4106@gmail.com>
2026-06-09 21:21:25 +0530

    Added Traiangle8.java

---
+++ PatternPrograms/Triangle8.java
@@ -0,0 +1,35 @@
+package com.gqt;
+
+import java.util.Scanner;
+
+public class Triangle8 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = scan.nextInt();
+		for (int i = 1; i <= n; i++) {
+
+			for (int k = 1; k <= i; k++) {
+				System.out.print("  ");
+			}
+			System.out.print("* ");
+			for (int j = 1; j <= 2 * (n - i) - 1; j++) {
+				System.out.print("  ");
+			}
+			if (i != n) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+//Output
+//Enter the number of rows : 5
+//*               * 
+//  *           * 
+//    *       * 
+//      *   * 
+//        * 
+
