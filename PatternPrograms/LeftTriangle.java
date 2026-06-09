e493dc5dea706c4bfb3e266c1a91e64e5b2676f3
darshan335 <darshan4106@gmail.com>
2026-06-09 21:06:30 +0530

    Added Left Traiangle.java

---
+++ PatternPrograms/LeftTriangle.java
@@ -0,0 +1,30 @@
+package com.gqt;
+
+import java.util.Scanner;
+
+public class LeftTriangle {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = scan.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int k = 0; k <= n - i; k++) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+//Output
+//-----------
+//Enter the number of rows : 5
+//         * 
+//       * * 
+//     * * * 
+//   * * * * 
+// * * * * * 
