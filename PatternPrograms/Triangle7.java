38f30e7f80efeba5baad648a9d4b61c34f35648e
darshan335 <darshan4106@gmail.com>
2026-06-09 21:21:04 +0530

    Added Traiangle7.java

---
+++ PatternPrograms/Triangle7.java
@@ -0,0 +1,42 @@
+package com.gqt;
+
+import java.util.Scanner;
+
+public class Triangle7 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = scan.nextInt();
+		for (int i = 1; i <= n - 1; i++) {
+			for (int k = 0; k <= n - i; k++) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for (int i = 1; i <= n; i++) {
+			for (int k = 1; k <= i; k++) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= 2 * (n - i) + 1; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+//Output
+//Enter the number of rows : 5
+//            * 
+//          * * * 
+//        * * * * * 
+//      * * * * * * * 
+//    * * * * * * * * * 
+//      * * * * * * * 
+//        * * * * * 
+//          * * * 
+//            * 
