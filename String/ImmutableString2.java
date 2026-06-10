7873b877f22e8f3b677aa8b13360d539ac9a137d
darshan335 <darshan4106@gmail.com>
2026-06-10 17:46:58 +0530

    Added String2

---
+++ String/ImmutableString2.java
@@ -0,0 +1,16 @@
+package com.string;
+
+public class ImmutableString2 {
+
+	public static void main(String[] args) {
+		String s1 = new String("Rama");
+		String s2 = new String("Rama");
+		if(s1==s2)
+			System.out.println("References are equal");
+		else
+			System.out.println("References are not equal");
+		
+
+	}
+
+}
