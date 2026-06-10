2eef2a5d70205e6152ff8962354158ca89ffa1ad
darshan335 <darshan4106@gmail.com>
2026-06-10 17:49:26 +0530

    Added String 7

---
+++ String/immutableString7.java
@@ -0,0 +1,12 @@
+package com.string;
+
+public class immutableString7 {
+
+	public static void main(String[] args) {
+		String s1="Rama";
+		System.out.println(s1);
+		s1.concat("Sita");
+		System.out.println(s1);
+	}
+
+}
