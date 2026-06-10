d663e07c871eceedfc87503a7124224ebe4160eb
darshan335 <darshan4106@gmail.com>
2026-06-10 17:52:22 +0530

    Local Variable

---
+++ String/LocalVariable.java
@@ -0,0 +1,16 @@
+package com.string;
+
+public class LocalVariable {
+
+	public static void main(String[] args) {
+		int a=25;
+		int b;
+		System.out.println(a);
+		if(a==25)
+			b=12;
+		else
+			b=32; 
+		System.out.println(b);
+	}
+
+}
