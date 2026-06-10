fbe421605a3b8cca8c6ea9e36d58a3b830c636f5
darshan335 <darshan4106@gmail.com>
2026-06-10 17:50:13 +0530

    Added Mutable String

---
+++ String/MutableString.java
@@ -0,0 +1,11 @@
+package com.string;
+
+public class MutableString {
+
+	public static void main(String[] args) {
+		StringBuffer s1 = new StringBuffer("Rama");
+		s1.append("Sita");
+		System.out.println(s1);
+	}
+
+}
