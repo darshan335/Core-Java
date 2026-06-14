ca7f3f3c7f7919db3a324029e48ac872ec23040e
darshan335 <darshan4106@gmail.com>
2026-06-14 19:17:16 +0530

    Added Abstraction

---
+++ Abstraction/Abstraction1.java
@@ -0,0 +1,15 @@
+package com.abstraction;
+abstract class Demo12{
+	//public void m1();//error
+	abstract public void m2();//error class should be abstrct
+	//abstraction is the process of hiding implementation and exposing only required funcationalty to end user
+	abstract public void m4();
+	
+}
+public class Abstraction1 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
