0505169e38c522f961b4e677d4e22addbdd499a7
darshan335 <darshan4106@gmail.com>
2026-06-15 09:33:33 +0530

    Added Multiple Inheritance

---
+++ Inheritance/MultipleInheritance.java
@@ -0,0 +1,22 @@
+package com.inheritance;
+class Sum{
+	public void m1() {
+		System.out.println("Parent");
+	}
+}
+class Sum2{
+	public void m1() {
+		System.out.println("2nd class");
+	}
+}
+//class New extends Sum,Sum2{
+//	
+//}
+public class MultipleInheritance {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+//
