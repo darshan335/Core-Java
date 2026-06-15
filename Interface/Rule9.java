89d8a7c3a85faa44906c1cb520a47edaf5ddef88
darshan335 <darshan4106@gmail.com>
2026-06-15 09:38:01 +0530

    Added Interface Rule 9

---
+++ Interface/Rule9.java
@@ -0,0 +1,20 @@
+//A class can extend another class and also can implement another interface. It is valid in java however class should extends first and implement next
+package com.interfaces;
+class Boss{
+	
+}
+interface I13{
+	void m1();
+}
+class Boss2 extends Boss implements I13{
+	public void m1() {
+		
+	}
+}
+public class Rule9 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
