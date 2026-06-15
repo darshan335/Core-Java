a09a9f4431a8ae48ac994927b230e01af5ec95f7
darshan335 <darshan4106@gmail.com>
2026-06-15 09:36:15 +0530

    Added Interface Rule 3

---
+++ Interface/Rule3.java
@@ -0,0 +1,16 @@
+//One interface cannot implement another interface
+package com.interfaces;
+interface Testing2{
+	void m1();
+}
+//interface I2 implements Testing2{
+//	
+//}
+public class Rule3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
