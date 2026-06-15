c2225b3bae5130765fede74092c77f519fc078f6
darshan335 <darshan4106@gmail.com>
2026-06-15 09:37:13 +0530

    Added Interface Rule 6

---
+++ Interface/Rule6.java
@@ -0,0 +1,13 @@
+//Rule 6 : Empty interface is permited in java
+package com.interfaces;
+interface I7{
+	
+}
+public class Rule6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
