af1a1a0c9bfd0144da3266816a5d3178907584c7
darshan335 <darshan4106@gmail.com>
2026-06-15 09:36:34 +0530

    Added Interface Rule 4

---
+++ Interface/Rule4.java
@@ -0,0 +1,16 @@
+//Rule 4: However one interface cannnot implement other interface but one interface can inherit from another interface
+package com.interfaces;
+interface I3{
+	void m1();
+}
+interface I4 extends I3{
+	
+}
+public class Rule4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
