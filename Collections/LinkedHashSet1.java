e03fbccbf2b790f4fd1b2cc45a4bb9b33eaa6c13
darshan335 <darshan4106@gmail.com>
2026-06-24 11:49:51 +0530

    Added Linked Hashset

---
+++ Collections/LinkedHashSet1.java
@@ -0,0 +1,16 @@
+package com.collections;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSet1 {
+
+	public static void main(String[] args) {
+		LinkedHashSet<Integer> l = new LinkedHashSet<>();
+		l.add(10);
+		l.add(40);
+		l.add(20);
+		l.add(30);
+		System.out.println(l);
+	}
+
+}
