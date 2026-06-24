23f5198be3e7263f605174319323ab0ed43e59a8
darshan335 <darshan4106@gmail.com>
2026-06-24 11:51:43 +0530

    Added Priority Quee 1

---
+++ Collections/PriorityQueue1.java
@@ -0,0 +1,16 @@
+package com.collections;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueue1 {
+
+	public static void main(String[] args) {
+		PriorityQueue p = new PriorityQueue<>();
+		p.add(100);
+		p.add(20);
+		p.add(5);
+		p.add(60);
+		System.out.println(p);
+	}
+
+}
