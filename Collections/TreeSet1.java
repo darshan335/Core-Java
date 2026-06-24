7cda84ebbcd2e94b0f17863a493f0fa1b4befe8b
darshan335 <darshan4106@gmail.com>
2026-06-24 11:52:12 +0530

    Added Tree Set 1

---
+++ Collections/TreeSet1.java
@@ -0,0 +1,16 @@
+package com.collections;
+
+import java.util.TreeSet;
+
+public class TreeSet1 {
+
+	public static void main(String[] args) {
+		TreeSet t = new TreeSet<>();
+		t.add(10);
+		t.add(87);
+		t.add(85);
+		t.add(5);
+		System.out.println(t);
+	}
+
+}
