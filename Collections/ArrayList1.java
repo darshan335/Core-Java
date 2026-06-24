09a5274f29e4f2a1bdfa332bbbb755e205956889
darshan335 <darshan4106@gmail.com>
2026-06-24 11:45:19 +0530

    Added collections

---
+++ Collections/ArrayList1.java
@@ -0,0 +1,15 @@
+package com.collections;
+
+import java.util.ArrayList;
+
+public class ArrayList1 {
+
+	public static void main(String[] args) {
+		ArrayList all = new ArrayList();
+		all.add(10);
+		all.add("Hi");
+		all.add(5.76);
+		System.out.println(all);
+	}
+
+}
