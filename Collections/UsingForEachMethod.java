e0edeb4932b25cd9e484fa7aa449e17a110501f2
darshan335 <darshan4106@gmail.com>
2026-06-24 11:53:44 +0530

    Added Using For Each Method

---
+++ Collections/UsingForEachMethod.java
@@ -0,0 +1,19 @@
+package com.collections;
+
+import java.util.ArrayList;
+
+public class UsingForEachMethod {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> l = new ArrayList<>();
+		l.add(10);
+		l.add(540);
+		l.add(30);
+		l.add(50);
+		l.add(130);
+		l.add(1043);
+		System.out.println(l);
+		l.forEach(i -> System.out.println(i));
+	}
+
+}
