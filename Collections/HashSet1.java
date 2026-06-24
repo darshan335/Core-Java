8abde2e21740e105f429918277166eb6e5e095d4
darshan335 <darshan4106@gmail.com>
2026-06-24 11:48:54 +0530

    Added hashset

---
+++ Collections/HashSet1.java
@@ -0,0 +1,17 @@
+package com.collections;
+
+import java.util.HashSet;
+
+public class HashSet1 {
+
+	public static void main(String[] args) {
+		HashSet h= new HashSet<>();
+		h.add(100);
+		h.add(60);
+		h.add(30);
+		h.add(5);
+		h.add(35);
+		System.out.println(h);
+	}
+
+}
